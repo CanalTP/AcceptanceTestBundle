cc6f5401321f77617851ee23070573cd28b49de9
Vincent Catillon <vincent.catillon@canaltp.fr>
2014-11-19 18:02:26 +0100

    Created the generic context V1 for acceptance tests

--- Mink/GenericContext.php
+++ Mink/GenericContext.php
@@ -6,5 +6,76 @@ use Behat\MinkExtension\Context\MinkContext;
 
 class GenericContext extends MinkContext
 {
+    /**
+     * @Given /^(?:|I am )logged as "(?P<role>(?:[^"]|\\")*)"$/
+     */
+    public function logAs($role)
+    {
+        switch ($role) {
+            case 'super_admin':
+                break;
+            case 'admin':
+                break;
+            case 'visitor':
+                break;
+            case 'translator':
+                break;
+            case 'user':
+                break;
+        }
+    }
     
+    /**
+     * Click on element with specified CSS
+     * 
+     * @When /^(?:|I )click on "(?P<id>(?:[^"]|\\")*)"$/
+     */
+    public function clickOn($element)
+    {
+        $this->assertSession()->elementExists('css', $element)->click();
+    }
+    
+    /**
+     * Checks, that element with specified CSS is visible on page.
+     *
+     * @Then /^(?:|The )"(?P<element>[^"]*)" element (should be|is) visible$/
+     */
+    public function assertElementVisible($element)
+    {
+        if (!$this->assertSession()->elementExists('css', $element)->isVisible()) {
+            throw new \Exception('Element "'.$element.'" not visible.');
+        }
+    }
+    
+    /**
+     * Checks, that element with specified CSS is not visible on page.
+     *
+     * @Then /^(?:|The )"(?P<element>[^"]*)" element (should not be|is not) visible$/
+     */
+    public function assertElementNotVisible($element)
+    {
+        if ($this->assertSession()->elementExists('css', $element)->isVisible()) {
+            throw new \Exception('Element "'.$element.'" visible.');
+        }
+    }
+    
+    /**
+     * Checks, that element childrens with specified CSS are visible on page.
+     */
+    public function assertElementChildrensVisible($element, $childrens = array())
+    {
+        foreach ($childrens as $children) {
+            $this->assertElementVisible($element.' '.$children);
+        }
+    }
+    
+    /**
+     * Checks, that element childrens with specified CSS are not visible on page.
+     */
+    public function assertElementChildrensNotVisible($element, $childrens = array())
+    {
+        foreach ($childrens as $children) {
+            $this->assertElementNotVisible($element.' '.$children);
+        }
+    }
 }
